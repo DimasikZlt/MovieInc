c917a27bb9116aa0e423adc932772b5a0953fd35
DimasikZlt <dimasikzlt@mail.ru>
2020-10-18 22:36:39 +0700

    Add tests Timetable and Movie classes

--- movie_inc/main.py
+++ movie_inc/main.py
@@ -1,6 +1,18 @@
+from movie import Movie
 from movie_house import MovieHouse
+from timetable import Timetable
 from tools.yaml_loader import load_yaml
 
 # movie_hall_schema = load_yaml('../data/cronverk.yml')
-movie_hall_schema = MovieHouse.create_movie_house(load_yaml, '../data/cronverk.yml')
+movie_house = MovieHouse.create_movie_house(load_yaml, '../data/cronverk.yml')
+movies_db = Movie.create_movies_db(load_yaml, '../data/movies.yml')
+timetable = Timetable.create_movies_timetable(movies_db, movie_house)
+for item in timetable:
+    movie_duration = item.movie.duration.seconds
+    minutes, hours = movie_duration // 60 % 60, movie_duration // 3600
+    print(
+        f"{item.movie.movie_title} - {item.movie_hall.number}, start: "
+        f"{item.start_time.strftime('%d.%m.%Y, %H:%M:%S')}, "
+        f"{hours}:{minutes}"
+    )
 print()
