188ebc5634cc3879b00beadbbe3cae1e91db0b8d
DimasikZlt <dimasikzlt@mail.ru>
2020-10-18 22:39:01 +0700

    Optimize loop over MovieHall

--- movie_inc/movie_house.py
+++ movie_inc/movie_house.py
@@ -1,13 +1,18 @@
-from typing import Callable
+from typing import Callable, List
 
 from movie_hall import MovieHall
 
 
 class MovieHouse:
-    def __init__(self):
-        pass
+    def __init__(self, name: str, address: str, movie_halls: List[MovieHall]):
+        self.name = name
+        self.address = address
+        self.movie_halls = movie_halls
 
     @classmethod
     def create_movie_house(cls, loader: Callable, movie_halls_schema_file: str):
         movie_halls_schema = loader(movie_halls_schema_file)
-        return MovieHall.create_movie_hall(movie_halls_schema['movie_halls'][0])
+        movie_halls = [
+            MovieHall.create_movie_hall(number) for number in movie_halls_schema['movie_halls']
+        ]
+        return cls(movie_halls_schema['name'], movie_halls_schema['address'], movie_halls)
